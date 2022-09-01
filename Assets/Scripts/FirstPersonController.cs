5c9828cbab6df76b35c6d412bf1ddb660d5aedad
nahon1 <60366732+nahon1@users.noreply.github.com>
2022-09-01 17:51:06 -0400

    Added player controls

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstPersonController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class FirstPersonController : MonoBehaviour{
+ 
+    private void Update() {
+
+        float forward = Input.GetAxis("Vertical") * Time.deltaTime * 10f;
+        float sideways = Input.GetAxis("Horizontal") * Time.deltaTime * 10f;
+
+        float turn = Input.GetAxis("Mouse X") * Time.deltaTime * 500f;
+        float tilt = Input.GetAxis("Mouse Y") * Time.deltaTime * -500f;
+
+        float currentFacing = transform.eulerAngles.y + turn;
+        //float currentTilt = cam.localEulerAngles.x + tilt;
+
+        transform.eulerAngles = new Vector3(0, currentFacing, 0);
+
+        transform.position += transform.TransformDirection(new Vector3(sideways, 0, forward));
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
